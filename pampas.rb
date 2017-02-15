8cb197b4677fa5b102111d302cdd8e29201c4829
gengrong <gr@terminus.io>
2017-02-15 17:35:35 +0800

    update version

--- pampas.rb
+++ pampas.rb
@@ -2,8 +2,8 @@ class Pampas < Formula
   desc "A "
   homepage "https://www.terminus.io/"
   url "http://terminus-paas.oss.aliyuncs.com/pampas.tar.gz"
-  version "0.0.2"
-  sha256 "d41c13358cf6c56aff3ab8dddbfd2f8e551db9c061243a1333977b7bc1444b6f"
+  version "0.0.3"
+  sha256 "29ad47bc1ad490e6495b62e8155973befc0b00e9ab6eb05aa985dc55c12ddff2"
 
   def install
     system "ls"
