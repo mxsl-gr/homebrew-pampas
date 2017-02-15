f1c9ec8cd15a501d2941cd0b537c9df8154b663f
gengrong <gr@terminus.io>
2017-02-15 17:19:34 +0800

    update brew

--- pampas.rb
+++ pampas.rb
@@ -6,11 +6,16 @@ class Pampas < Formula
   sha256 "2cefe05c181d1d65df070586436e1f8324ec42d1ab660c2f792a934550a8ac4a"
 
   def install
+    system "ls"
     # system "make install"
   end
 
   def post_install
     # (HOMEBREW_PREFIX/"bin").install_symlink bin/"python2.6"
+  end
+
+  test do
+    system "#{bin}/pampas", "version"
   end
 
 end
