c42a061b831cfe8951f3ca937c008b8b3e2664bd
gengrong <gr@terminus.io>
2017-02-15 18:19:02 +0800

    update brew

--- pampas.rb
+++ pampas.rb
@@ -3,14 +3,14 @@ class Pampas < Formula
   homepage "https://www.terminus.io/"
   url "http://terminus-paas.oss.aliyuncs.com/pampas.tar.gz"
   version "0.0.3"
-  sha256 "29ad47bc1ad490e6495b62e8155973befc0b00e9ab6eb05aa985dc55c12ddff2"
+  sha256 "8824d0babf1a77c66d05921535c3323fcbe31350dd7b096e92e6947be78fe5f6"
 
   def install
     libexec.install Dir["*"]
-    Pathname.glob("#{libexec}/bin/*") do |file|
-      next if file.directory?
-      basename = file.basename
-    end
+    # Pathname.glob("#{libexec}/bin/*") do |file|
+    #   next if file.directory?
+    #   basename = file.basename
+    # end
   end
 
   def post_install
