a7e2ec30d5610a751bd783c73d8a0314ac9f94bd
gengrong <gr@terminus.io>
2017-03-28 13:33:14 +0800

    update code

--- pampas.rb
+++ pampas.rb
@@ -3,7 +3,7 @@ class Pampas < Formula
   homepage "https://www.terminus.io/"
   url "http://terminus-paas.oss.aliyuncs.com/dist/pampas/pampas.0.1.0.tar.gz"
   version "0.1.0"
-  sha256 "2db68536baae7e9c2545d5e1b78e8936a2194bf2d7b1b79d5b4ee74aebc21ee1"
+  sha256 "d394aa9fb64537acdabf8264b484be8bceb4285b777e1e9d3ed65a0bc509c93d"
 
   depends_on "maven"
   depends_on "git"
