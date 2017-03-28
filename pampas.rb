14a0d3764af7da7d6e5235a42e0847ffbb759cf1
gengrong <gr@terminus.io>
2017-03-28 13:31:32 +0800

    add caveats

--- pampas.rb
+++ pampas.rb
@@ -3,7 +3,7 @@ class Pampas < Formula
   homepage "https://www.terminus.io/"
   url "http://terminus-paas.oss.aliyuncs.com/dist/pampas/pampas.0.1.0.tar.gz"
   version "0.1.0"
-  sha256 "6e4910457f4a8931e0e27ef97517280b2a72c9d9560f0e59622a242c287f2fe9"
+  sha256 "2db68536baae7e9c2545d5e1b78e8936a2194bf2d7b1b79d5b4ee74aebc21ee1"
 
   depends_on "maven"
   depends_on "git"
@@ -13,8 +13,16 @@ class Pampas < Formula
   def install
     bin.install Dir["bin/*"]
     lib.install Dir["lib/*"]
-    ENV["PAMPAS_TEMPLATES"] = HOMEBREW_PREFIX/"pampas_templates/"
+    ENV["PAMPAS_TEMPLATES"] = HOMEBREW_PREFIX/"pampas-templates/"
     ENV["BUILDPACK_PATH"] = HOMEBREW_PREFIX/"buildpacks/"
+  end
+
+  def caveats
+    install_script = HOMEBREW_PREFIX/"scripts/install-mac.sh"
+    <<-EOS.undent
+      run shell script (need root):
+        #{install_script}
+    EOS
   end
 
   test do
